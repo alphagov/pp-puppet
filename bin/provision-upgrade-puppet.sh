a839b0ddb5b9800b4c5588c53e8ec872b5c57e51
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-05-24 22:37:02 +0100

    Clean up output of bin/provision-upgrade-puppet.sh

--- bin/provision-upgrade-puppet.sh
+++ bin/provision-upgrade-puppet.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+echo "... Installing the correct librarian-puppet and facter versions"
 # Remove puppet 2.7.19 which is installed on this basebox
-gem uninstall puppet -v '2.7.19'
-gem uninstall facter -v '1.6.17'
+gem uninstall puppet -v '2.7.19' >/dev/null
+gem uninstall facter -v '1.6.17' >/dev/null
 # Install a decent version of Facter and Librarian-Puppet from Gems
-gem install --no-rdoc --no-ri --version "0.9.8" librarian-puppet
-gem install --no-rdoc --no-ri --version "1.7.1" facter
+gem install --no-rdoc --no-ri --version "0.9.8" librarian-puppet >/dev/null
+gem install --no-rdoc --no-ri --version "1.7.1" facter >/dev/null
