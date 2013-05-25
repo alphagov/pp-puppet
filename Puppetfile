bce0ecf8ad3e26dee4ee2fcc7a5599af2513bef2
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-05-25 10:52:17 +0100

    Install jenkins on the Deploy machine

--- Puppetfile
+++ Puppetfile
@@ -2,6 +2,7 @@ forge "http://forge.puppetlabs.com"
 
 mod 'attachmentgenie/ssh'
 mod 'attachmentgenie/ufw'
+mod "jenkins", :git => "git://github.com/jenkinsci/puppet-jenkins.git"
 mod 'puppetlabs/gcc'
 mod 'puppetlabs/stdlib'
 mod 'torrancew/account'
--- manifests/machines/deploy.pp
+++ manifests/machines/deploy.pp
@@ -1,3 +1,10 @@
 class machines::deploy inherits machines::base {
     notify { 'Included the Deploy class': }
+    # Install Jenkins
+    include jenkins
+    ufw::allow { "allow-jenkins-from-jumpbox":
+        port => 8080,
+        ip   => 'any',
+        from => '10.0.0.11'
+    }
 }
