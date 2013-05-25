46c4d25d330f894655d72316155ef0db3f63dd1d
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-05-25 16:05:02 +0100

    Fix missing commas in UFW rules

--- manifests/machines/frontend_app.pp
+++ manifests/machines/frontend_app.pp
@@ -2,13 +2,13 @@ class machines::frontend_app inherits machines::base {
     notify { 'Included the Frontend-App class': }
     ufw::allow { "allow-http-from-frontend-lb-1":
         port => 80,
-        ip   => 'any'
-        from => $hosts['frontend-lb-1']['ip']
+        ip   => 'any',
+        from => $hosts['frontend-lb-1']['ip'],
     }
     ufw::allow { "allow-https-from-frontend-lb-1":
         port => 443,
-        ip   => 'any'
-        from => $hosts['frontend-lb-1']['ip']
+        ip   => 'any',
+        from => $hosts['frontend-lb-1']['ip'],
     }
     include nginx::server
 }
