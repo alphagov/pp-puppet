e8b6e60f3bd7004b133fee271896681ca33056e6
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-05-25 18:16:39 +0100

    Add NginX config for frontend and backend app servers

--- manifests/machines/backend_app.pp
+++ manifests/machines/backend_app.pp
@@ -10,4 +10,16 @@ class machines::backend_app inherits machines::base {
         from => $hosts['backend-lb-1.backend']['ip'],
     }
     include nginx::server
+    nginx::vhost::proxy { 'backdrop-read-vhost':
+        port            => 80,
+        servername      => join(['read.backdrop',hiera("domain_name")],'.'),
+        ssl             => false,
+        upstream_port   => 3038,
+    }
+    nginx::vhost::proxy { 'backdrop-write-vhost':
+        port            => 80,
+        servername      => join(['write.backdrop',hiera("domain_name")],'.'),
+        ssl             => false,
+        upstream_port   => 3039,
+    }
 }
--- manifests/machines/frontend_app.pp
+++ manifests/machines/frontend_app.pp
@@ -10,4 +10,10 @@ class machines::frontend_app inherits machines::base {
         from => $hosts['frontend-lb-1.frontend']['ip'],
     }
     include nginx::server
+    nginx::vhost::proxy { 'limelight-vhost':
+        port            => 80,
+        servername      => join(['limelight',hiera("domain_name")],'.'),
+        ssl             => false,
+        upstream_port   => 3040,
+    }
 }
