da75fa6cd5372d73849023bb6cb02d4af15155b3
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-05-25 17:13:22 +0100

    Open SSH everywhere in development

--- manifests/machines/base.pp
+++ manifests/machines/base.pp
@@ -2,6 +2,7 @@ class machines::base {
     # Store the list of hosts for use later
     $hosts = hiera('hosts')
     $networks = hiera('networks')
+    $environment = hiera('environment')
 
     exec { 'apt-get-update':
         command => '/usr/bin/apt-get update || true',
@@ -13,18 +14,27 @@ class machines::base {
 FACTER_machine_class=${machine_class}
 "
     }
-
     # Default the firewall to closed
     include ufw
-    ufw::allow { "allow-ssh-from-jumpbox-1":
-        port => 22,
-        ip   => 'any',
-        from => $hosts['jumpbox-1.management']['ip'],
-    }
-    ufw::allow { "allow-ssh-from-deploy-1":
-        port => 22,
-        ip   => 'any',
-        from => $hosts['deploy-1.management']['ip'],
+    # Open up SSH everywhere in development
+    if $environment == 'development' {
+        # This is necessary for Vagrant ssh to work
+        ufw::allow { "allow-ssh-from-anywhere":
+            port => 22,
+            ip   => 'any',
+        }
+    } else {
+        # Default the firewall to closed
+        ufw::allow { "allow-ssh-from-jumpbox-1":
+            port => 22,
+            ip   => 'any',
+            from => $hosts['jumpbox-1.management']['ip'],
+        }
+        ufw::allow { "allow-ssh-from-deploy-1":
+            port => 22,
+            ip   => 'any',
+            from => $hosts['deploy-1.management']['ip'],
+        }
     }
 
     # Secure SSHd to prevent root login and only allow keys
