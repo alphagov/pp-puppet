a20464659d82c44bdf63d785691c475c100bfdf5
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-05-25 11:37:00 +0100

    Install an NginX proxy on the jumpbox for the deploy server

--- manifests/machines/jumpbox.pp
+++ manifests/machines/jumpbox.pp
@@ -1,3 +1,18 @@
 class machines::jumpbox inherits machines::base {
     notify { 'Included the Jumpbox class': }
+    include nginx
+    include nginx::server
+    # Proxy Jenkins to the Deploy Server
+    ufw::allow { "allow-http-from-all":
+        port => 80,
+        ip   => 'any'
+    }
+    nginx::vhost::proxy { 'deploy-vhost':
+        port            => 80,
+        servername      => join(['deploy',hiera("domain_name")],'.'),
+        ssl             => false,
+        template        => 'nginx/vhost-proxy.conf.erb',
+        upstream_server => 'deploy-1.management',
+        upstream_port   => 8080,
+    }
 }
