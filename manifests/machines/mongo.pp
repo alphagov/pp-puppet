c831a1dded1f42a861f8926fd2d9a6634ba0411d
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-05-25 15:01:39 +0100

    Auto-configure replica-set This does the framework for replicaset configuration. However it requires that all nodes are up and running Mongo first, so if you bring up nodes one at a time, it will probably fail on the first two. It is likely to fail on the 3rd node as well, because mongo might have started, but is not actually available by the time the script runs. To fix this (before we have a cronjob to run puppet), simply do "vagrant provision mongo-1.backend"

--- manifests/machines/mongo.pp
+++ manifests/machines/mongo.pp
@@ -7,6 +7,53 @@ class machines::mongo inherits machines::base {
     }
     class { 'mongodb':
         enable_10gen => true,
-        replSet      => 'production'
+        replSet      => 'production',
+        logpath      => '/var/log/mongodb/mongodb.log',
+        dbpath       => '/var/lib/mongodb'
+    }
+    file { '/etc/mongodb':
+        ensure  => 'directory',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+    }
+    $mongo_hosts = grep(keys($hosts),'mongo')
+    $mongo_members_tmp = join($mongo_hosts,'","')
+    $mongo_members = "[\"${mongo_members_tmp}\"]"
+    file { '/etc/mongodb/configure-replica-set.js':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        require => File['/etc/mongodb'],
+        content => "
+function replicaSetMembers() {
+  var members = ${mongo_members};
+  var i = 0;
+  return members.map(function(member) {
+    return {
+      _id: i++,
+      host: member
+    };
+  })
+}
+
+function replicaSetConfig() {
+  return {
+    _id: 'production',
+    members: replicaSetMembers()
+  };
+}
+
+rs.initiate(replicaSetConfig());
+"
+    }
+    exec { 'configure-replica-set':
+      command => "/usr/bin/mongo --host ${mongo_hosts[0]} /etc/mongodb/configure-replica-set.js",
+      unless  => "/usr/bin/mongo --host ${mongo_hosts[0]} --quiet --eval 'rs.status().ok' | grep -q 1",
+      require => [
+        File['/etc/mongodb/configure-replica-set.js'],
+        Service['mongodb'],
+      ],
     }
 }
