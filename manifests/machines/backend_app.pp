4ae681c958b9ab83d1a6b274823c6c2a701c7f9f
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-05-25 16:16:42 +0100

    Fix a mismatch in machine hostnames

--- manifests/machines/backend_app.pp
+++ manifests/machines/backend_app.pp
@@ -3,12 +3,12 @@ class machines::backend_app inherits machines::base {
     ufw::allow { "allow-http-from-backend-lb-1":
         port => 80,
         ip   => 'any',
-        from => $hosts['backend-lb-1']['ip'],
+        from => $hosts['backend-lb-1.backend']['ip'],
     }
     ufw::allow { "allow-https-from-backend-lb-1":
         port => 443,
         ip   => 'any',
-        from => $hosts['backend-lb-1']['ip'],
+        from => $hosts['backend-lb-1.backend']['ip'],
     }
     include nginx::server
 }
--- manifests/machines/frontend_app.pp
+++ manifests/machines/frontend_app.pp
@@ -3,12 +3,12 @@ class machines::frontend_app inherits machines::base {
     ufw::allow { "allow-http-from-frontend-lb-1":
         port => 80,
         ip   => 'any',
-        from => $hosts['frontend-lb-1']['ip'],
+        from => $hosts['frontend-lb-1.frontend']['ip'],
     }
     ufw::allow { "allow-https-from-frontend-lb-1":
         port => 443,
         ip   => 'any',
-        from => $hosts['frontend-lb-1']['ip'],
+        from => $hosts['frontend-lb-1.frontend']['ip'],
     }
     include nginx::server
 }
