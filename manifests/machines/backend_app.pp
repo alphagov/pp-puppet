f4dbc8e0f1d095852e18a8f78e43490438856709
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-05-30 16:53:00 +0100

    Set up backdrop on backend app machines.

--- manifests/machines/backend_app.pp
+++ manifests/machines/backend_app.pp
@@ -11,16 +11,41 @@ class machines::backend_app inherits machines::base {
         from => $hosts['backend-lb-1.backend']['ip'],
     }
     include nginx::server
-    nginx::vhost::proxy { 'backdrop-read-vhost':
-        port            => 80,
-        servername      => join(['read.backdrop',hiera('domain_name')],'.'),
-        ssl             => false,
-        upstream_port   => 3038,
+    user { 'deploy':
+        ensure => present,
     }
-    nginx::vhost::proxy { 'backdrop-write-vhost':
-        port            => 80,
-        servername      => join(['write.backdrop',hiera('domain_name')],'.'),
-        ssl             => false,
-        upstream_port   => 3039,
+    class { 'python':
+        version    => '2.7',
+        dev        => true,
+        virtualenv => true,
     }
+    file { '/var/virtualenvs':
+        ensure => directory,
+        owner  => 'deploy',
+    }
+    include upstart
+    file { '/etc/gds':
+        ensure => directory,
+        owner  => 'deploy',
+    }
+    backdrop::app {'read.backdrop':
+        port       => 3038,
+        app_module => 'backdrop.read.api:app',
+    }
+    backdrop::app {'write.backdrop':
+        port       => 3039,
+        app_module => 'backdrop.write.api:app',
+    }
+
+
+    # Manual deployment steps
+    # 
+    # Copy backdrop source to /opt/read.backdrop and /opt/write.backdrop
+    # Install python dependencies (from project root)
+    #    sudo -u deploy /var/virtualenvs/{title}/bin/pip install -r requirements.txt
+    # Install configuration files
+    #    I'd really like to get these moved out of the package hierarchy
+    #    Copy from alphagov-deployment to /opt/{title}/backdrop/{read,write}/config/production.py
+    # For write.backdrop
+    #    sudo -u deploy backdrop/write/config/development_tokens.py backdrop/write/config/tokens.py
 }
