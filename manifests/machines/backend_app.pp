cdb78e43df26f99960a153fb1c4cf2c29bc8e19d
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-06-05 16:19:21 +0100

    Tidy up backend app manifest
    Move more of the setup down into puppet-backdrop.

--- manifests/machines/backend_app.pp
+++ manifests/machines/backend_app.pp
@@ -10,42 +10,31 @@ class machines::backend_app inherits machines::base {
         ip   => 'any',
         from => $hosts['backend-lb-1.backend']['ip'],
     }
-    include nginx::server
+
     user { 'deploy':
         ensure => present,
     }
+
     class { 'python':
         version    => '2.7',
         dev        => true,
         virtualenv => true,
     }
-    file { '/var/virtualenvs':
-        ensure => directory,
-        owner  => 'deploy',
-    }
-    include upstart
-    file { '/etc/gds':
-        ensure => directory,
-        owner  => 'deploy',
-    }
+
     backdrop::app {'read.backdrop':
         port       => 3038,
         app_module => 'backdrop.read.api:app',
+        domain_name => hiera('domain_name'),
+        user        => 'deploy',
+        group       => 'deploy',
     }
+
     backdrop::app {'write.backdrop':
         port       => 3039,
         app_module => 'backdrop.write.api:app',
+        domain_name => hiera('domain_name'),
+        user        => 'deploy',
+        group       => 'deploy',
     }
 
-
-    # Manual deployment steps
-    # 
-    # Copy backdrop source to /opt/read.backdrop and /opt/write.backdrop
-    # Install python dependencies (from project root)
-    #    sudo -u deploy /var/virtualenvs/{title}/bin/pip install -r requirements.txt
-    # Install configuration files
-    #    I'd really like to get these moved out of the package hierarchy
-    #    Copy from alphagov-deployment to /opt/{title}/backdrop/{read,write}/config/production.py
-    # For write.backdrop
-    #    sudo -u deploy backdrop/write/config/development_tokens.py backdrop/write/config/tokens.py
 }
