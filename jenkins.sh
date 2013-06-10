3b061a5d305fa8fe2713a4892eb4ea510dd7dfeb
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-06-10 12:12:23 +0100

    Add documentation to jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 set -xe
+# Install gems in bundler
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
-
+# Run librarian puppet to download modules
 bundle exec librarian-puppet install --strip-dot-git
+# RUN ALL THE TESTS
 bundle exec rake test
 # Remove old debfiles
 rm -rf build/*
+# Build the deb package
 bundle exec rake deb
+echo "Built package: pp_puppet_${BUILD_NUMBER}"
