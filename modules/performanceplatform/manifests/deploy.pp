f4ad9ad02a3d98957273328fc6d92a71ec1707aa
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2013-06-18 09:08:21 +0100

    Turn on basic auth if flag exists

--- modules/performanceplatform/manifests/deploy.pp
+++ modules/performanceplatform/manifests/deploy.pp
@@ -2,6 +2,7 @@ class performanceplatform::deploy (
     $vpn_gateway  = undef,
     $vpn_user     = undef,
     $vpn_password = undef,
+    $basic_auth   = undef,
 ) {
     #Only phone home if the vpn credentials are set
     if ( $vpn_gateway and ( $vpn_user and $vpn_password )) {
@@ -9,6 +10,20 @@ class performanceplatform::deploy (
             gateway   => $vpn_gateway,
             user      => $vpn_user,
             password  => $vpn_password,
+        }
+    }
+    if ($basic_auth) {
+        file { '/etc/nginx/htpasswd.pp':
+            ensure  => present,
+            content => $basic_auth,
+            require => Package['nginx'],
+        }
+        file { '/etc/nginx/conf.d/basic_auth.conf':
+            ensure  => present,
+            content => 'auth_basic "Enter username/password";
+auth_basic_user_file /etc/nginx/htpasswd.pp;',
+            require => File['/etc/nginx/htpasswd.pp'],
+            notify  => Service['nginx'],
         }
     }
 }
