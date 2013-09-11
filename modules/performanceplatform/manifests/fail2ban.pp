23061a8c6c4e3dc40142a2f2c027eeb5eb8c0899
Ralph Cowling <ralph.cowling@digital.cabinet-office.gov.uk>
2013-09-11 16:01:30 +0100

    5min ban after 3 retries, change name to pp_fail2ban to avoid re-declaration conflicts/errors

--- modules/performanceplatform/manifests/fail2ban.pp
+++
@@ -1,9 +0,0 @@
-# Class: performanceplatform::fail2ban
-#
-#
-class performanceplatform::fail2ban {
-    $whitelist_ips = hiera('whitelist_ips')
-    class { 'fail2ban':
-        jails_template => content('performanceplatform/jail.local.erb')
-    }
-}
