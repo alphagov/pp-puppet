4161898dc07c20b5bd8c3880408be0107f94eaba
Ralph Cowling <ralph.cowling@digital.cabinet-office.gov.uk>
2013-09-11 14:14:19 +0100

    conf template

--- modules/performanceplatform/manifests/fail2ban.pp
+++ modules/performanceplatform/manifests/fail2ban.pp
@@ -3,9 +3,8 @@
 #
 class performanceplatform::fail2ban {
     # TODO - shove hierdata ref in here
-    # $whitelistips = hiera('some_value_you_have_put_in_common.yaml', '127.0.0.1')
+    $whitelist_ips = hiera('environment.yaml', '127.0.0.1')
     class { 'fail2ban':
-        # TODO make this template
         jails_template => content('performanceplatform/jail.local.erb')
 
       # jails_config => 'file',
