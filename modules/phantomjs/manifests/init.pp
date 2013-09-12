f152affb86bba8ac539f283a6baae70b9bf7331e
Tom Booth <tombooth@gmail.com>
2013-09-12 12:13:25 +0100

    [#55989114] Switch to using alphagov/puppet-curl
    Our curl module is a lot cleaner than execing the curl command itself so
    I have changed to using the module. This is also needed for logstash.

--- modules/phantomjs/manifests/init.pp
+++ modules/phantomjs/manifests/init.pp
@@ -1,12 +1,11 @@
 class phantomjs {
 
-  exec { 'download phantomjs':
-    command => '/usr/bin/curl -o phantomjs-1.9.1-linux-x86_64.tar.bz2 https://gds-public-readable-tarballs.s3.amazonaws.com/phantomjs-1.9.1-linux-x86_64.tar.bz2',
-    cwd     => '/usr/local/src',
-    creates => '/usr/local/src/phantomjs-1.9.1-linux-x86_64.tar.bz2',
-    require => Package['curl'],
-    timeout => 3600,
-    unless  => '/usr/bin/test "`shasum phantomjs-1.9.1-linux-x86_64.tar.bz2`" = "8ab4753abd352eaed489709c6c7dd13dae67cd91 phantomjs-1.9.1-linux-x86_64.tar.bz2"'
+  include curl
+
+  curl::fetch { 'download phantomjs':
+    source      => 'https://gds-public-readable-tarballs.s3.amazonaws.com/phantomjs-1.9.1-linux-x86_64.tar.bz2',
+    destination => '/usr/local/src/phantomjs-1.9.1-linux-x86_64.tar.bz2',
+    timeout     => 3600,
   }
 
   exec { 'unpack phantomjs':
