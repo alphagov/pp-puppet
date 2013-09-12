1e7ff503249eef6739896047e56b1cdd8c99361e
Ralph Cowling <ralph.cowling@digital.cabinet-office.gov.uk>
2013-09-12 12:19:02 +0100

    have a jails example, sub case the whitelist ips variable for clarity

--- modules/performanceplatform/manifests/pp_fail2ban.pp
+++ modules/performanceplatform/manifests/pp_fail2ban.pp
@@ -2,7 +2,7 @@
 #
 #
 class performanceplatform::pp_fail2ban {
-    $whitelist_ips = hiera('whitelist_ips')
+    $whitelist_ips = hiera('fail2ban::whitelist_ips')
     class { 'fail2ban':
         jails_config => 'file',
         jails_template => 'performanceplatform/jail.local.erb'
