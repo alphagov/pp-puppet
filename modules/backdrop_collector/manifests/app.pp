850281772f26e86fb2c3e8e553e648756f5b369d
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2013-09-18 13:46:35 +0100

    Lint

--- modules/backdrop_collector/manifests/app.pp
+++ modules/backdrop_collector/manifests/app.pp
@@ -7,19 +7,19 @@ define backdrop_collector::app (
     $log_path = "/var/log/${title}"
     $config_path = "/etc/gds/${title}"
 
-    file { ["$log_path", "$config_path", "$app_path", "$app_path/releases", "$app_path/shared", "$app_path/shared/log"]:
+    file { [$log_path, $config_path, $app_path, "${app_path}/releases", "${app_path}/shared", "${app_path}/shared/log"]:
         ensure  => directory,
         owner   => $user,
         group   => $group,
         recurse => true,
     }
 
-    python::virtualenv { "$virtualenv_path":
+    python::virtualenv { $virtualenv_path:
         ensure     => present,
         version    => '2.7',
         systempkgs => false,
         owner      => $user,
         group      => $group,
-        require    => File["$app_path/shared"],
+        require    => File["${app_path}/shared"],
     }
 }
