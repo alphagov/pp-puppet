c71d1b370533f88e8a1dd5a778debc879f9d3b88
Guy Koth <gkoth@thoughtworks.com>
2013-10-04 16:12:05 +0100

    Tidy up and name change

--- modules/performanceplatform/manifests/backdrop_checks.pp
+++ modules/performanceplatform/manifests/backdrop_checks.pp
@@ -1,7 +1,8 @@
 class performanceplatform::backdrop_checks (
 ) {
     $check_data_path = '/etc/sensu/community-plugins/plugins/http/check-http.rb'
-      sensu::check { application_heath_check:
+
+    sensu::check { abackdrop_heath_check:
       command  => "${check_data_path}  -u http://localhost/_status",
-      }
+    }
 }
