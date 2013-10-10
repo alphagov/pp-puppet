bed4cf32c274a3c53d8f5e62cc61158c1e9fa558
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2013-10-10 13:25:14 +0100

    Add Node.js to the frontend machines and dev VM

---
+++ modules/performanceplatform/manifests/nodejs.pp
@@ -0,0 +1,11 @@
+class performanceplatform::nodejs {
+    package { 'nodejs':
+        ensure => "0.10.20-1chl1~${::lsbdistcodename}1",
+        require => Apt::Ppa['ppa:gds/performance-platform']
+    }
+    package { 'grunt-cli':
+        ensure   => '0.1.9',
+        provider => 'npm',
+        require  => Package['nodejs'],
+    }
+}
