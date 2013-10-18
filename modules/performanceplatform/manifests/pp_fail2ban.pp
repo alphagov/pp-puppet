a8341627b9f2238389a4d50e2560e9ec6ce56c3c
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-10-18 11:57:17 +0100

    Send fail2ban logs to syslog

--- modules/performanceplatform/manifests/pp_fail2ban.pp
+++ modules/performanceplatform/manifests/pp_fail2ban.pp
@@ -4,7 +4,8 @@
 class performanceplatform::pp_fail2ban {
     $whitelist_ips = hiera('fail2ban::whitelist_ips')
     class { 'fail2ban':
+        source         => 'puppet:///modules/performanceplatform/fail2ban.local',
         jails_config   => 'file',
-        jails_template => 'performanceplatform/jail.local.erb'
+        jails_template => 'performanceplatform/jail.local.erb',
     }
 }
