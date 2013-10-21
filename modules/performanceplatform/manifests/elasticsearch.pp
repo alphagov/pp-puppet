047e02e3a7ffbf623867724373d2ba0b237b916d
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-10-21 12:56:17 +0100

    Rotate elasticsearch logs and add es head
    - Rotate elasticsearch logs for kibana keeping previous 21 days

---
+++ modules/performanceplatform/manifests/elasticsearch.pp
@@ -0,0 +1,17 @@
+class performanceplatform::elasticsearch {
+  include ::elasticsearch
+
+  cron {'elasticsearch-rotate-indices':
+    ensure  => present,
+    user    => 'nobody',
+    hour    => '0',
+    minute  => '1',
+    command => '/usr/local/bin/es-rotate --delete-old --delete-maxage 21 --optimize-old --optimize-maxage 1 logs',
+    require => Class['::elasticsearch'],
+  }
+
+  elasticsearch::plugin { 'head':
+    install_from => 'mobz/elasticsearch-head',
+  }
+
+}
