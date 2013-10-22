5d9d790c3c7bb5cb8447d6228906eda991ba5c91
Paulo Schneider <paulo.schneider@gmail.com>
2013-10-22 10:08:53 +0100

    Removing lint warnings

--- modules/performanceplatform/manifests/backdrop_smoke_tests.pp
+++ modules/performanceplatform/manifests/backdrop_smoke_tests.pp
@@ -10,13 +10,13 @@ class performanceplatform::backdrop_smoke_tests (
 
     file { "/etc/sensu/backdrop-write-read-test.rb":
       require => Class['sensu'],
-      owner => root,
-      group => root,
-      mode  => 444,
-      source => "puppet:///modules/performanceplatform/backdrop-write-read-test.rb"
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0444',
+      source  => "puppet:///modules/performanceplatform/backdrop-write-read-test.rb"
     }
 
-    sensu::check { backdrop_smoke_tests:
+    sensu::check { 'backdrop_smoke_tests':
       interval => 120,
       command  => "ruby ${check_data_path}  -u 'https://${www_vhost}/test' -b'b6be45f57635955b99742bda9970639d3982e52c690d052c6856beb991f865f25b3b87290acf7e1f09927f5ab4c2b6a24c91cc2a01f547e72d0864247ba868ca'"
     }
