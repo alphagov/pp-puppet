0c853140da8a457ecc085f3ce39b0e2edf6fe4d0
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-10-23 17:40:27 +0100

    Tidy up backdrop read/write smoke test
    - Improve error messages
    - Fix indentation
    - Handle the case where the read api response is valid json but not
      formatted how we expect

--- modules/performanceplatform/files/backdrop-write-read-test.rb
+++ modules/performanceplatform/files/backdrop-write-read-test.rb
@@ -25,52 +25,49 @@ class BackdropWriteReadTest< Sensu::Plugin::Check::CLI
   auth = config[:auth_token]
   content_type = "application/json"
   begin
-  write = Curl::Easy.http_post(uri.to_s, payload.to_json) do |curl|
-    curl.headers['Content-Type'] = 'application/json'
-    curl.headers['Authorization'] = "Bearer #{auth}"
-    curl.headers['Content-Type'] = content_type
-    curl.ssl_verify_peer = false
-  end
-  if  write.body.to_s.include? "ok"
-    sleep(SLEEP_CONST)
+    write = Curl::Easy.http_post(uri.to_s, payload.to_json) do |curl|
+      curl.headers['Content-Type'] = 'application/json'
+      curl.headers['Authorization'] = "Bearer #{auth}"
+      curl.headers['Content-Type'] = content_type
+      curl.ssl_verify_peer = false
+    end
+    if  write.body.to_s.include? "ok"
+      sleep(SLEEP_CONST)
 
-    uri_sorting_and_results_limitation = config[:url] + "?sort_by=_timestamp:descending&limit=1"
-    read = Curl::Easy.http_get(uri_sorting_and_results_limitation) do |curlinfo|
-      curlinfo.ssl_verify_peer = false
-    end
+      uri_sorting_and_results_limitation = config[:url] + "?sort_by=_timestamp:descending&limit=1"
+      read = Curl::Easy.http_get(uri_sorting_and_results_limitation) do |curlinfo|
+        curlinfo.ssl_verify_peer = false
+      end
 
-    begin
-      read_api_response = JSON.parse(read.body)
-    rescue JSONError => json_error
-      print "Could not parse Json from response body" + json_error
-      critical "Json Parse Failed from read API"
-    end
-    begin
-      read_api_time_stamp = Time.parse(read_api_response['data'][0]['_timestamp']).utc
-    rescue TimeError => time_error
-      print "Error parsing time from read api response : " + time_error
-      critical "Time parse error from the read api response"
-    end
-    begin
-      utc_time = Time.parse(payload["_timestamp"]).utc
-    rescue TimeError => payload_time_error
-      print "Error parsing time from the local payload : " + payload_time_error
-      critical "Time parsing error from local payload"
-    end
-    if (utc_time - read_api_time_stamp) < API_TIME_TO_WRITE
-      ok "We write and read information from the API"
+      begin
+        read_api_response = JSON.parse(read.body)
+        read_api_timestamp = read_api_response['data'][0]['_timestamp']
+      rescue JSONError, IndexError
+        critical "Failed to parse JSON from read API"
+      end
+      begin
+        read_api_timestamp = Time.parse(read_api_timestamp).utc
+      rescue TimeError
+        critical "Failed to parse time from read API '#{read_api_timestamp}'"
+      end
+      begin
+        utc_time = Time.parse(payload["_timestamp"]).utc
+      rescue TimeError
+        critical "Failed to parse time from local payload '#{payload['_timestamp']}'"
+      end
+      if (utc_time - read_api_time_stamp) < API_TIME_TO_WRITE
+        ok "Succeeded in writing and reading from backdrop"
+      else
+        critical "Failed to read latest record from the read API"
+      end
+
     else
-      puts (utc_time - read_api_time_stamp)
-      critical "we could not read the content from the read API"
+      critical "Failed to write to the write API";
     end
 
-  else
-    critical "Could not use the write API";
-  end
-
-  end
+    end
   rescue
-    critical "Something went really wrong "
+    critical "Something went really wrong"
   end
 end
 
