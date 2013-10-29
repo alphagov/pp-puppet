c483daad3cb50a16fdcb9ed6c69cc3a299d09449
Ralph Cowling <ralph.cowling@digital.cabinet-office.gov.uk>
2013-10-29 17:50:24 +0000

    @wip add global to regsubst

--- modules/performanceplatform/manifests/server_checking.pp
+++ modules/performanceplatform/manifests/server_checking.pp
@@ -2,7 +2,7 @@ class performanceplatform::server_checking(
   $boxes,
 ) {
 
-  $domain = regsubst($::fqdn, '[\.]+\.(.*)', '\1')
+  $domain = regsubst($::fqdn, '[\.]+\.(.*)', '\1', 'G')
 
   performanceplatform::server_checks{ $boxes:
     domain => $domain,
