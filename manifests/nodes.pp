76bace7da112552733d537e4195e7b2542df086f
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2013-11-18 12:34:49 +0000

    Add screenshot-as-a-service application
    Creates screenshots that can be used by Spotlight for fallback images.
    
    eg Spotlight can call http://screenshot_as_a_service/?url=http://localhost:3057/dashboard-url

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -18,6 +18,7 @@ $logging_vhost       = join(['logging',$domain_name],'.')
 $alerts_vhost        = join(['alerts',$domain_name],'.')
 $www_vhost           = join(['www',$public_domain_name],'.')
 $spotlight_vhost     = join(['spotlight',$public_domain_name],'.')
+$screenshot_as_a_service_vhost = join(['screenshot', $public_domain_name], '.')
 
 $rabbitmq_sensu_password = hiera('rabbitmq_sensu_password')
 
