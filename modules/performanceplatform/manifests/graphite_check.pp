830c9f4ba2978fb39c98860f4190d60bd24c8aac
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-11-19 11:13:49 +0000

    Upgrade sensu community plugins
    - Provides graphite check with a flag to ignore http errors.

--- modules/performanceplatform/manifests/graphite_check.pp
+++ modules/performanceplatform/manifests/graphite_check.pp
@@ -5,21 +5,29 @@ define performanceplatform::graphite_check(
   $interval = 60,
   $handlers = undef,
   $ignore_no_data = false,
+  $ignore_http_error = false,
 ) {
 
   $check_data_path = '/etc/sensu/community-plugins/plugins/graphite/check-data.rb'
   $server_config = '-s graphite -u betademo -p nottobes'
 
   if $ignore_no_data {
-    $flags = '--ignore-no-data'
+    $ignore_no_data_flag = '--ignore-no-data'
   } else {
-    $flags = ''
+    $ignore_no_data_flag = ''
   }
+
+  if $ignore_http_error {
+    $ignore_http_error_flag = '--ignore-http-error'
+  } else {
+    $ignore_http_error_flag = ''
+  }
+
 
   $max_age = $interval * 2
 
   sensu::check { $name:
-    command  => "${check_data_path} ${server_config} ${flags} --age ${max_age} -t \"${target}\" -w \"${warning}\" -c \"${critical}\" -n \"${name}\"",
+    command  => "${check_data_path} ${server_config} ${ignore_no_data_flag} ${ignore_http_error_flag} --age ${max_age} -t \"${target}\" -w \"${warning}\" -c \"${critical}\" -n \"${name}\"",
     interval => $interval,
     handlers => $handlers,
     custom   => {
