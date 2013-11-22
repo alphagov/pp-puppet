c2fffed7fca3576e59604a9898307e53b2155783
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2013-11-22 22:21:05 +0000

    Add assets vhost to the frontend machines
    Assets will be served from assets.env.pp-domain.
    
    This commit adds a vhost listening on 80 and 443 on that subdomain to
    the frontend machines, with a root inside /var/www/{subdomain} (by default).
    
    It then adds some extra nginx config to the end of that server block
    so that all requests under /spotlight are served from the public
    directory of the current Spotlight deployment.
    
    assets.perfplat.dev/spotlight/index.css -> /opt/spotlight/current/public/index.css
    
    We set 2 hour cache times and send an etag. Once we've verified that we're doing
    proper cachebusting in the app, we can increase that Expires header to months or
    years.

---
+++ modules/performanceplatform/manifests/assets.pp
@@ -0,0 +1,15 @@
+# == Class: performanceplatform::assets
+#
+# This class provides an nginx vhost that serves assets from
+# a dedicated assets subdomain. You should be able to set far
+# future cache headers on your assets here.
+class performanceplatform::assets (
+) {
+
+  nginx::vhost { 'assets-vhost':
+    servername => "${::assets_vhost}",
+    ssl        => true,
+    magic      => template('performanceplatform/assets-vhost.erb'),
+  }
+
+}
