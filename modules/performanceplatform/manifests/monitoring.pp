b59603c7668de075a7e511fd8478fda4a03a12e2
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2013-11-22 17:05:59 +0000

    Filter out log messages to backdrop _status
    Varnish healthcheck hits it a lot.

--- modules/performanceplatform/manifests/monitoring.pp
+++ modules/performanceplatform/manifests/monitoring.pp
@@ -80,6 +80,21 @@ class performanceplatform::monitoring (
     ],
   }
 
+  logstash::filter::grep { 'ignore_backdrop_status_request':
+    match  => {
+      '@message' => "\"(request|response): GET .*/_status( - 200 OK)?\"",
+    },
+    negate => true,
+    order  => 20,
+  }
+  logstash::filter::grep { 'ignore_gunicorn_status_request':
+    match  => {
+      '@message' => "\\\"GET /_status HTTP/1.0\\\"",
+    },
+    negate => true,
+    order  => 20,
+  }
+
   logstash::output::statsd { 'statsd':
     type      => 'lumberjack',
     tags      => [ 'nginx' ],
