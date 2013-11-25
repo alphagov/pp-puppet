4f60069cfcb5a3fbd80146b723d3f3d1b99da739
James Abley <james.abley@gmail.com>
2013-11-25 10:33:02 +0000

    Update elasticsearch check
    Elasticsearch has moved from monitoring-1 to 2 new boxes. Our checks
    need updating as a result.

--- modules/performanceplatform/manifests/elasticsearch.pp
+++ modules/performanceplatform/manifests/elasticsearch.pp
@@ -79,4 +79,10 @@ class performanceplatform::elasticsearch(
     }'
   }
 
+  sensu::check { 'elasticsearch_is_out_of_memory':
+    command  => '/etc/sensu/community-plugins/plugins/files/check-tail.rb -f /var/log/elasticsearch/elasticsearch.log -l 50 -P OutOfMemory',
+    interval => 60,
+    handlers => 'pagerduty',
+  }
+
 }
