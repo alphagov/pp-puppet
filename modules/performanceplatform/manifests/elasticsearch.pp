3a542d1fedd363e21f718136e741eb185a5c6a59
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2014-01-03 14:56:45 +0000

    Remove es-rotate optimization util available
    It can be added back in when
    https://github.com/gds-operations/puppet-elasticsearch/pull/9 is merged.

--- modules/performanceplatform/manifests/elasticsearch.pp
+++ modules/performanceplatform/manifests/elasticsearch.pp
@@ -32,7 +32,7 @@ class performanceplatform::elasticsearch(
     user    => 'nobody',
     hour    => '0',
     minute  => '1',
-    command => '/usr/local/bin/es-rotate --delete-old --delete-maxage 21 --optimize-old --optimize-maxage 1 logstash',
+    command => '/usr/local/bin/es-rotate --delete-old --delete-maxage 21 logstash',
     require => Class['::elasticsearch'],
   }
 
