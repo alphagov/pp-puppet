2056640ad17cd79a28d4b670f26b7a53ddc1ffa5
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2014-01-07 12:53:04 +0000

    Add logrotate config for backdrop gunicorn logs
    - Gunicorn logs are growing indefinitely
    - The postrotate is nasty but upstart doesn't give you a way of sending
      a custom signal

--- modules/backdrop/manifests/app.pp
+++ modules/backdrop/manifests/app.pp
@@ -43,6 +43,12 @@ define backdrop::app (
         group   => $group,
         content => template('backdrop/gunicorn.logging.conf.erb')
     }
+    file { "/etc/logrotate.d/${title}-gunicorn":
+      ensure  => present,
+      owner   => $user,
+      group   => $group,
+      content => template('backdrop/gunicorn.logrotate.erb'),
+    }
     file { "${app_path}/run-procfile.sh":
         ensure  => present,
         owner   => $user,
