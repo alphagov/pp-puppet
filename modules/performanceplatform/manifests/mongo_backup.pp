6f889505e316a5ba5cd178dbf3f82244b58a1f0d
Ralph Cowling <ralph.cowling@digital.cabinet-office.gov.uk>
2014-03-04 10:59:59 +0000

    Add in mongo backup class
    Quite a dumb interim solution, but should be useful:
     - Add backup manifest that writes to whichever box the class is added to
     - Create a backup log, and write success/fail to it on backup
     - Add a sensu check that checks for the existence of 'failure' in the log
     - Add the backup class to the mongo box role

---
+++ modules/performanceplatform/manifests/mongo_backup.pp
@@ -0,0 +1,68 @@
+# Class: mongo::backup
+#
+# Parameters:
+#   ['backup_dir'] - The directory to use for backups.
+#
+#   ['user']       - User writing out the backups.
+#
+#   ['backup_log'] - The log file to write success/failure of backing up.
+#
+# Requires:
+# - `puppetlabs/stdlib`
+#
+# Sample Usage:
+#   include mongodb::backup
+#
+class performanceplatform::mongo_backup (
+  $backup_dir  = '/var/backups/mongodb',
+  $backup_log_dir  = '/var/log/backups/',
+  $backup_log  = '/var/log/backups/mongodb.log',
+  $user        = 'root',
+) inherits performanceplatform::mongo {
+
+  file {$backup_dir:
+    ensure  => directory,
+    owner   => $user,
+    group   => $user,
+    mode    => '0755',
+  }
+
+  file {$backup_log_dir:
+    ensure  => directory,
+    owner   => $user,
+    group   => $user,
+    mode    => '0755',
+  }
+
+  file {$backup_log:
+    ensure  => present,
+    owner   => $user,
+    group   => $user,
+    mode    => '0777',
+  }
+
+  file { '/usr/local/bin/mongo-backup.sh':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    content => template('performanceplatform/mongo-backup.sh.erb'),
+    require => File[$backup_dir],
+  }
+
+  cron { 'mongo-backup':
+    command => "/usr/local/bin/mongo-backup.sh",
+    user    => $user,
+    hour    => 3,
+    minute  => 0,
+    require => File['/usr/local/bin/mongo-backup.sh'],
+  }
+
+  sensu::check { 'mongodb_backed_up_less_than_24hrs_ago':
+    command  => '/etc/sensu/community-plugins/plugins/files/check-tail.rb -f /var/log/backups/mongodb.log -l 1 -P failed',
+    interval => 86400,
+    handlers => ['default'],
+  }
+
+
+}
