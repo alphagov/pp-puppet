eda3b3da9989aa5862b9ff5422407dac725ced45
Ralph Cowling <ralph.cowling@digital.cabinet-office.gov.uk>
2014-03-24 11:56:18 +0000

    Add in mount configuration for backup_box
    - Should add in '/mnt/data/backup' directory
    
    ensure present and tidy up

--- modules/performanceplatform/manifests/backup_box.pp
+++ modules/performanceplatform/manifests/backup_box.pp
@@ -1,7 +1,11 @@
 # Class: performanceplatform::backup_box
 #
 #
-class performanceplatform::backup_box {
+class performanceplatform::backup_box(
+    $data_dir,
+    $disk_mount,
+) {
+
 
     lvm::volume { 'data':
         ensure => 'present',
@@ -9,6 +13,18 @@ class performanceplatform::backup_box {
         pv     => '/dev/sdb1',
         fstype => 'ext4',
     }
+
+    file { '/mnt/data':
+        ensure => directory,
+    }
+
+    performanceplatform::mount { $data_dir:
+        mountoptions => 'defaults',
+        disk         => $disk_mount,
+        require      => File['/mnt/data'],
+    }
+
+
 
 }
     # mapping aiming to conform to pattern lv-vg
--- modules/performanceplatform/spec/classes/backup_box_spec.rb
+++ modules/performanceplatform/spec/classes/backup_box_spec.rb
@@ -1,17 +1,23 @@
 require_relative '../../../../spec_helper'
 
 describe 'performanceplatform::backup_box', :type => :class do
+
+    let (:params) {{
+        'data_dir'      => '/mnt/data/backup',
+        'disk_mount'    => '/dev/mapper/data-backup',
+    }}
+
     it { should contain_lvm__volume('data').with(
+        :ensure => 'present',
         :vg     => 'backup',
         :pv     => '/dev/sdb1',
         :fstype => 'ext4',
     )}
+
+    it { should contain_file('/mnt/data').with_ensure(
+        "directory"
+    )}
+
+    it { should contain_performanceplatform__mount('/mnt/data/backup') }
+
 end
-
-
-# describe 'govuk::lvm', :type => :define do
-#   let(:title) { 'purple' }
-
-#   context 'with no params' do
-#     it { expect { should contain_lvm__volume('purple') }.to raise_error(Puppet::Error, /Must pass pv/) }
-#   end
