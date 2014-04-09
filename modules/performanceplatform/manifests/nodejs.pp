09b006b7bee1aa0963fe4b29e82ebfbc489a621d
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-04-09 15:43:56 +0100

    Update Node.js from 0.10.21 to 0.10.26
    I have copied the package to the relevant PPA.

--- modules/performanceplatform/manifests/nodejs.pp
+++ modules/performanceplatform/manifests/nodejs.pp
@@ -1,6 +1,6 @@
 class performanceplatform::nodejs {
   package { 'nodejs':
-    ensure  => "0.10.21-1chl1~${::lsbdistcodename}1",
+    ensure  => "0.10.26-1chl1~${::lsbdistcodename}1",
     require => [Apt::Ppa['ppa:gds/performance-platform'], Exec['apt-get-update']],
   }
   package { 'grunt-cli':
