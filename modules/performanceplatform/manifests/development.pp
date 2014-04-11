eb4ebe441606bd1bc52316fd4080f305424fb6cf
Dan Carley <dan.carley@gmail.com>
2014-04-11 13:56:44 +0100

    Source virtualenvwrapper for development-1
    Replaces another difference in `tools/bootstrap-vagrant` from
    alphagov/pp-development@9adefc2. I've set `replace => false` to only create
    the file if it doesn't exist, in order to match existing behaviour.

--- modules/performanceplatform/manifests/development.pp
+++ modules/performanceplatform/manifests/development.pp
@@ -1,6 +1,14 @@
 class performanceplatform::development (
   $stagecraft_password
 ) {
+  file { '/home/vagrant/.bash_profile':
+    ensure  => file,
+    replace => false,
+    owner   => 'vagrant',
+    group   => 'vagrant',
+    source  => 'puppet:///modules/performanceplatform/home/vagrant/.bash_profile',
+  }
+
   postgresql::server::role { 'stagecraft':
     createdb      => true,
     password_hash => postgresql_password('stagecraft', $stagecraft_password),
