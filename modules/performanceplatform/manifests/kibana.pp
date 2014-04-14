alphagov/pp-puppet

commit 99a4887434594a0b24f004bb1bf579a4122eb2cf

Force download of new Kibana tarball
Earlier commit a44090a13 did not work on preview. This was due to the fact
that the archive resource had the same name so puppet skipped over
downloading the new tarball. Giving the archive resource a name that
includes the version forces a new download to take place.

modules/performanceplatform/manifests/kibana.pp
@@ -12,7 +12,7 @@ class performanceplatform::kibana(
   $tarball_name = regsubst($tarball_url, $name_regex, '\1')
   $app_root = "${extract_location}/${tarball_name}"
 
-  archive { 'kibana':
+  archive { 'kibana3.0.1':
     ensure   => present,
     url      => $tarball_url,
     target   => $extract_location,

commit 5e25f80d2ed30ff9b1a732ec12416674976d5837

Fix Kibana install requires
Fixes bug introduced by 99a4887.

The Nginx vhost and config file resources need to require the new name
for the archive resource.

modules/performanceplatform/manifests/kibana.pp
@@ -29,7 +29,7 @@ class performanceplatform::kibana(
   file { "${app_root}/config.js":
     ensure  => present,
     content => template('performanceplatform/kibana.config.js.erb'),
-    require => Archive['kibana'],
+    require => Archive['kibana3.0.1'],
   }
 
   nginx::vhost { 'kibana-vhost':
@@ -39,7 +39,7 @@ class performanceplatform::kibana(
     access_logs => {
       '{name}.access.log.json' => 'json_event',
     },
-    require     => Archive['kibana'],
+    require     => Archive['kibana3.0.1'],
   }
 
 }