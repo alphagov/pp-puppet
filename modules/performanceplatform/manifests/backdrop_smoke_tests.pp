f5cbbbe63ce3e62ca482b7855a4ca5524837722a
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2014-05-12 17:13:26 +0100

    Remove the old backdrop smoke test
    pp-smokey now reports to sensu so this is redundant.

--- modules/performanceplatform/manifests/backdrop_smoke_tests.pp
+++ modules/performanceplatform/manifests/backdrop_smoke_tests.pp
@@ -1,19 +1,12 @@
 class performanceplatform::backdrop_smoke_tests (
   $test_data_set_token='qwertyuiop'
 ) {
-    $check_data_path ="/etc/sensu/backdrop-write-read-test.rb"
-
     file { "/etc/sensu/backdrop-write-read-test.rb":
+      ensure  => absent,
       require => Class['sensu'],
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0444',
-      source  => "puppet:///modules/performanceplatform/backdrop-write-read-test.rb"
     }
 
     sensu::check { 'backdrop_smoke_tests':
-      interval => 120,
-      command  => "ruby ${check_data_path}  -u 'https://${::www_vhost}/data/test/test' -b'${test_data_set_token}'",
-      handlers => ['default', 'pagerduty'],
+      ensure => absent,
     }
 }
