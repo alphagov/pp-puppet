355b0cb7eb1c00093597809e8e84bce06a4c501c
Jacob Ashdown <jcbashdown@gmail.com>
2014-05-29 16:58:30 +0100

    Remove old collector repos and remove files from backend app boxes

--- modules/backdrop_collector/manifests/app.pp
+++ modules/backdrop_collector/manifests/app.pp
@@ -1,18 +1,25 @@
-define backdrop_collector::app ($user, $group) {
+define backdrop_collector::app ($user, $group, $ensure) {
     $app_path = "/opt/${title}"
     $virtualenv_path = "${app_path}/shared/venv"
     $log_path = "/var/log/${title}"
     $config_path = "/etc/gds/${title}"
 
+    if $ensure == 'present' {
+      $ensure_directory = 'directory' 
+    }
+    else {
+      $ensure_directory = 'absent' 
+    }
+
     file { [$log_path, $config_path, $app_path, "${app_path}/releases", "${app_path}/shared", "${app_path}/shared/log"]:
-        ensure  => directory,
+        ensure  => $ensure_directory,
         owner   => $user,
         group   => $group,
         recurse => true,
     }
 
     python::virtualenv { $virtualenv_path:
-        ensure     => present,
+        ensure     => $ensure,
         version    => '2.7',
         systempkgs => false,
         owner      => $user,
@@ -22,5 +29,6 @@ define backdrop_collector::app ($user, $group) {
 
     lumberjack::logshipper { "collector-logs-for-${title}":
         log_files => [ "${app_path}/current/log/collector.log.json" ],
+        ensure => $ensure
     }
 }
