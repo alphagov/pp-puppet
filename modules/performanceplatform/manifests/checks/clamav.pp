c6891e7cf261d5c1d6ce6fd316533aa817fb8d96
Jacob Ashdown <jcbashdown@gmail.com>
2014-07-04 15:51:41 +0100

    This check actually works to check whether the daemon is running
    the other was doing more than checking - it was also trying to do things
    it needed sudo for

--- modules/performanceplatform/manifests/checks/clamav.pp
+++ modules/performanceplatform/manifests/checks/clamav.pp
@@ -2,7 +2,7 @@ class performanceplatform::checks::clamav (
 ) {
 
   sensu::check { 'clamav_is_down':
-    command  => '/etc/init.d/clamav-daemon status',
+    command  => 'pgrep clamd',
     interval => 60,
     handlers => ['default']
   }
