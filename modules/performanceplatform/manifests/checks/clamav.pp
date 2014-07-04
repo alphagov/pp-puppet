a4d246b95436e5c9715490820357a24f4ce7342e
Jacob Ashdown <jcbashdown@gmail.com>
2014-07-04 16:53:41 +0100

    3rd time lucky? I want criticals and sensible output
    props to tombooth

--- modules/performanceplatform/manifests/checks/clamav.pp
+++ modules/performanceplatform/manifests/checks/clamav.pp
@@ -1,10 +1,19 @@
 class performanceplatform::checks::clamav (
 ) {
+    $check_clamav_running_script = "/etc/sensu/check_clamav_daemon_running.sh"
 
-  sensu::check { 'clamav_is_down':
-    command  => 'pgrep clamd',
-    interval => 60,
-    handlers => ['default']
-  }
+    file { $check_clamav_running_script:
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0777',
+      source  => "puppet:///modules/performanceplatform/check_clamav_daemon_running.sh"
+    }
+
+    sensu::check { 'clamav_is_down':
+      command  => "${check_clamav_running_script}",
+      interval => 60,
+      handlers => ['default']
+    }
 
 }
