4f7cd4d2fd9b298bb8494180641a3a77cd75394c
Tim Mower <timothy.mower@gmail.com>
2014-07-18 11:02:42 +0100

    Add a logrotate rule for elasticsearch
    compressing log files should help with disk space - and we dont need to
    keep logs going back to 2013...

--- modules/performanceplatform/manifests/elasticsearch.pp
+++ modules/performanceplatform/manifests/elasticsearch.pp
@@ -102,4 +102,14 @@ class performanceplatform::elasticsearch(
     handlers => ['default'],
   }
 
+  logrotate::rule { 'elasticsearch-rotate':
+    path         => '/var/log/elasticsearch/elasticsearch.log.*',
+    rotate       => 30,
+    rotate_every => 'day',
+    missingok    => true,
+    compress     => true,
+    create       => true,
+    create_mode  => '0640',
+  }
+
 }
