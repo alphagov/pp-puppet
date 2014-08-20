d585425cf80e1cd9a4f73293bf49f80dd286ae48
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2014-08-20 19:01:32 +0100

    Fix postgresql for pp-development
    This is down to a parse order issue introduced by renaming the
    postgresql module (and therefore making it come after the postgres dev
    module). Avoided by allowing the globals we need to override to be
    provided by hiera. I have chosen to put these variables in common even
    though they only apply to three roles to avoid duplication.

--- modules/pp_postgres/manifests/server.pp
+++ modules/pp_postgres/manifests/server.pp
@@ -1,11 +1,3 @@
 class pp_postgres::server {
-  Class['postgresql::globals'] -> Class['postgresql::server']
-  class { 'postgresql::globals':
-    # Don't install from the postgresql PPA. See "postgresql::globals" @
-    # See https://forge.puppetlabs.com/puppetlabs/postgresql#setup
-    manage_package_repo => false,
-    version             => '9.1',
-  }->
-  class { 'postgresql::server':
-  }
+  include('postgresql::server')
 }
