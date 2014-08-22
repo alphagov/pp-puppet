a343b0da5b8589af485cf96ee47d332e28133812
Tom Booth <tombooth@gmail.com>
2014-08-22 14:30:50 +0100

    Use the data_dir from postgresql module
    The puppetlabs module that we use has a data_dir that will be setup for
    the appropriate os and postgresql version. We should use that rather
    than maintaining our own.

--- modules/pp_postgres/manifests/secondary.pp
+++ modules/pp_postgres/manifests/secondary.pp
@@ -2,10 +2,9 @@ class pp_postgres::secondary {
   include('pp_postgres::server')
   include('pp_postgres::monitoring::secondary')
 
-  $data_dir = '/var/lib/postgresql/9.1/main'
   $primary_host = 'postgresql-primary'
 
-  file { "${data_dir}/recovery.conf":
+  file { "${::postgresql::params::data_dir}/recovery.conf":
     ensure  => present,
     content => "standby_mode = 'on'\nprimary_conninfo = 'host=postgresql-primary-1 user=replicator'",
     owner   => "postgres",
