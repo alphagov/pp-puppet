de5865fc147ddaa8b1e085352fb7c8201b05c320
Tim Mower <timothy.mower@gmail.com>
2014-09-01 17:08:51 +0100

    Check skew more frequently
    There have been a few alerts show up on clock skew, but by the time of
    checking them they are resolved. We may also need to tune the warning /
    critical threshold, but this change means minor clock skew shouldnt
    end up with critical production alerts for 1 hour after the case (until
    the check runs again)

--- modules/performanceplatform/manifests/checks/ntp.pp
+++ modules/performanceplatform/manifests/checks/ntp.pp
@@ -3,7 +3,7 @@ class performanceplatform::checks::ntp (
 
   sensu::check { "check_ntp":
     command  => '/etc/sensu/community-plugins/plugins/system/check-ntp.rb -w 2 -c 3',
-    interval => 3600,
+    interval => '60',
     handlers => ['default']
   }
 
