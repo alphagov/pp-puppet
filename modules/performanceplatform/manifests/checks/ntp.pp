ab55ed3289ae05a54f776f04b34c5f64cee0f658
James Abley <james.abley@gmail.com>
2014-09-05 10:20:19 +0100

    Use explicit defaults for NTP checks
    Using the defaults as in https://github.com/sensu/sensu-community-plugins/blob/c6cef387d980e17ba4e493f9cd48671c3b66ecf7/plugins/system/check-ntp.rb
    but having the explicitly here so that people don’t have to go looking
    elsewhere for the values.
    
    I’ve asked in the #sensu channel on freenode for more documentation
    about the the check-ntp plugin, but for now, this should quieten our
    alerts and we can tune them in the future.

--- modules/performanceplatform/manifests/checks/ntp.pp
+++ modules/performanceplatform/manifests/checks/ntp.pp
@@ -2,7 +2,7 @@ class performanceplatform::checks::ntp (
 ) {
 
   sensu::check { "check_ntp":
-    command  => '/etc/sensu/community-plugins/plugins/system/check-ntp.rb -w 2 -c 3',
+    command  => '/etc/sensu/community-plugins/plugins/system/check-ntp.rb -w 10 -c 100',
     interval => '60',
     handlers => ['default']
   }
