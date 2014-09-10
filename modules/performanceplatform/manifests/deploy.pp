8a321fe83113205794a42a22571e8ef1d379cfa7
James Abley <james.abley@gmail.com>
2014-09-10 11:50:49 +0100

    Revert change to deploy htpasswd
    Iterating on fixing puppet in preview.

--- modules/performanceplatform/manifests/deploy.pp
+++ modules/performanceplatform/manifests/deploy.pp
@@ -18,7 +18,7 @@ class performanceplatform::deploy (
         file { '/etc/nginx/htpasswd.pp':
             ensure  => present,
             content => $basic_auth,
-            require => Package['nginx-extra'],
+            require => Package['nginx'],
         }
     }
     if ($jenkins_key) {
