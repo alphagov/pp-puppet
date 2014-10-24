c641f2afc2deb17bfc171cfa0dc579922f076680
James Abley <james.abley@gmail.com>
2014-10-24 14:51:01 +0100

    lint

--- modules/performanceplatform/manifests/development.pp
+++ modules/performanceplatform/manifests/development.pp
@@ -23,7 +23,7 @@ class performanceplatform::development (
   }
 
   performanceplatform::development::environment { 'stagecraft':
-    postactivate       => 'export DJANGO_SETTINGS_MODULE=development',
+    postactivate      => 'export DJANGO_SETTINGS_MODULE=development',
     requirements_path => 'requirements/development.txt'
   }
 
