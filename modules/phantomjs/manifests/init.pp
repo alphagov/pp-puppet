cf13af64edbadb0babae86cfd57d8501c5f016ce
Tim Mower <timothy.mower@gmail.com>
2014-11-05 10:19:10 +0000

    Remove phantom cleanup

--- modules/phantomjs/manifests/init.pp
+++ modules/phantomjs/manifests/init.pp
@@ -4,9 +4,4 @@ class phantomjs {
     ensure => '1.9.7-0~ppa1',
   }
 
-  # FIXME: Remove when this has been run in all environments
-  file { '/usr/local/bin/phantomjs':
-    ensure  => absent,
-  }
-
 }
