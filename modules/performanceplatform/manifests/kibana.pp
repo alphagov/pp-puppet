9f1f090871b6c57a7e46aa848844f51bc430bd58
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2014-11-07 15:15:41 +0000

    Fix typo: ssl_dharam -> ssl_dhparam

--- modules/performanceplatform/manifests/kibana.pp
+++ modules/performanceplatform/manifests/kibana.pp
@@ -29,13 +29,13 @@ class performanceplatform::kibana(
   $ssl_dhparam = hiera('ssl_dhparam')
 
   nginx::resource::vhost { $::kibana_vhost:
-    ssl        => true,
-    ssl_cert   => "${ssl_path}/${ssl_cert}",
-    ssl_key    => "${ssl_path}/${ssl_key}",
-    ssl_dharam => "${ssl_path}/${ssl_dhparam}",
-    www_root   => $app_root,
-    access_log => "${::kibana_vhost}.access.log.json json_event",
-    require    => Archive[$kib_name],
+    ssl         => true,
+    ssl_cert    => "${ssl_path}/${ssl_cert}",
+    ssl_key     => "${ssl_path}/${ssl_key}",
+    ssl_dhparam => "${ssl_path}/${ssl_dhparam}",
+    www_root    => $app_root,
+    access_log  => "${::kibana_vhost}.access.log.json json_event",
+    require     => Archive[$kib_name],
   }
 
 }
