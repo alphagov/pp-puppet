ccf5631bb6f7814747470014202da2fa372df88f
Jacob Ashdown <jcbashdown@gmail.com>
2014-12-09 16:22:25 +0000

    Only check logging rate in production.
    Currently logging rate is so low in preview (and most likely staging)
    that it is constantly triggering a critical alert in those environments.
    
    This allows us to deploy to staging without constant critical alerts. If
    we find we need the log check back however, Tim has suggested adding a
    heartbeat.

--- modules/performanceplatform/manifests/checks/elasticsearch/logging.pp
+++ modules/performanceplatform/manifests/checks/elasticsearch/logging.pp
@@ -1,14 +1,17 @@
 class performanceplatform::checks::elasticsearch::logging(
 ) {
 
-  performanceplatform::checks::elasticsearch::index { 'check_lumberjack_logging_rate':
-    index => 'logstash-current',
-    type  => 'lumberjack',
-  }
+  
+  if $::pp_environment == 'production' {
+    performanceplatform::checks::elasticsearch::index { 'check_lumberjack_logging_rate':
+      index => 'logstash-current',
+      type  => 'lumberjack',
+    }
 
-  performanceplatform::checks::elasticsearch::index { 'check_syslog_logging_rate':
-    index => 'logstash-current',
-    type  => 'syslog',
+    performanceplatform::checks::elasticsearch::index { 'check_syslog_logging_rate':
+      index => 'logstash-current',
+      type  => 'syslog',
+    }
   }
 
 }
